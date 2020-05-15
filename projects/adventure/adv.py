439d81ba7c94e6c385e0a22252bac6936494a0cb
D-Fink <dfink4badjuju@gmail.com>
2020-05-15 10:38:16 -0600

    sprint mvp

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -10,11 +10,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "projects/adventure/maps/test_line.txt"
+# map_file = "projects/adventure/maps/test_cross.txt"
+# map_file = "projects/adventure/maps/test_loop.txt"
+# map_file = "projects/adventure/maps/test_loop_fork.txt"
+map_file = "projects/adventure/maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -28,7 +28,45 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+# visited room dict
+visited_rooms = {}
+# store the reverse path to backtrack
+path_finder = []
+# need opposite directions to backtrack
+reverse_dir ={'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+# get starting room and find the exits
+visited_rooms[player.current_room.id] = player.current_room.get_exits()
+# while we havent visited all the rooms
+while len(visited_rooms) < len(room_graph):
+    # if not visited
+    if player.current_room.id not in visited_rooms:
+        # mark as visited and get exits
+        visited_rooms[player.current_room.id] = player.current_room.get_exits()
+        # get the room we came from and remove it from list of exits
+        last_dir = path_finder[-1]
+        visited_rooms[player.current_room.id].remove(last_dir)
+    # when all paths in room have been visited
+    if len(visited_rooms[player.current_room.id]) == 0:
+        # backtrack until an unexplored path is found and pop as we go
+        last_dir = path_finder[-1]
+        path_finder.pop()
+        # add last direction to traversal_path
+        traversal_path.append(last_dir)
+        # move player in direction
+        player.travel(last_dir)
 
+    # if there are unexplored directions
+    else:
+        # define first available direction
+        direction = visited_rooms[player.current_room.id][-1]
+        # pop it from the list
+        visited_rooms[player.current_room.id].pop()
+        # append to the traversal_path
+        traversal_path.append(direction)
+        # track the path so we can backtrack
+        path_finder.append(reverse_dir[direction])
+        # move player in direction
+        player.travel(direction)
 
 
 # TRAVERSAL TEST
